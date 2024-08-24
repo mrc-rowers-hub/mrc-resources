dc21a582ef5d814db982789f2a5421a26f498788
AdelaideBaron <baronadelaide@hotmail.com>
2024-08-24 21:26:28 +0100

    date util

---
+++ src/main/java/com/codeaddi/mrc_resources/controller/util/DateUtil.java
@@ -0,0 +1,19 @@
+package com.codeaddi.mrc_resources.controller.util;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DateUtil {
+
+    public static Map<Date, LocalTime> getDateAndTimeFromLocalDateTime(LocalDateTime localDateTime) {
+        Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+        LocalTime time = localDateTime.toLocalTime();
+        Map<Date, LocalTime> map = new HashMap<>();
+        map.put(date, time);
+        return map;
+    }
+}
