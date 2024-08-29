498f9c94f5c24b303ebb6e506d9a090db7f3a366
AdelaideBaron <baronadelaide@hotmail.com>
2024-08-29 11:34:52 +0100

    filtering for resources in use on date

--- src/main/java/com/codeaddi/mrc_resources/controller/service/ResourceService.java
+++ src/main/java/com/codeaddi/mrc_resources/controller/service/ResourceService.java
@@ -3,6 +3,7 @@ package com.codeaddi.mrc_resources.controller.service;
 import com.codeaddi.mrc_resources.controller.service.db.BladeService;
 import com.codeaddi.mrc_resources.controller.service.db.ResourceInUseService;
 import com.codeaddi.mrc_resources.controller.util.DateUtil;
+import com.codeaddi.mrc_resources.model.http.enums.ResourceStatus;
 import com.codeaddi.mrc_resources.model.repository.entity.Blade;
 import com.codeaddi.mrc_resources.model.repository.entity.ResourceInUse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,8 @@ public class ResourceService {
     ResourceInUseService resourceInUseService;
 
 //    public List<Blade> getBladesAvailableAtTime(LocalDateTime localDateTime){
+    // change to be 'getResourceAvailableAtTime' and then have a method for filtering boats and blades
+
     public void getBladesAvailableAtTime(LocalDateTime localDateTime){
         Map<Date, LocalTime> dateTime = DateUtil.getDateAndTimeFromLocalDateTime(localDateTime);
         Date date = dateTime.keySet().stream().findAny().orElseThrow();
@@ -35,11 +38,25 @@ public class ResourceService {
         if(anyInUseOnDate(date, bladesInUse)){
             // find out the times
             // if that is within 2 hours of this session, return notification of
+        } else {
+
         }
     }
 
     public boolean anyInUseOnDate(Date date, List<ResourceInUse> bladesInUse){
         return bladesInUse.stream().anyMatch(blade -> blade.getDate().equals(date));
+    }
+
+    public List<ResourceInUse> getThoseInUseOnDate(Date date, List<ResourceInUse> bladesInUse){
+        return bladesInUse.stream().filter(blade -> blade.getDate().equals(date)).toList();
+    }
+//
+//    public List<ResourceUseDTO> getResourcesInUseDTOForAvailableBlades(List<Blade> blades){
+//
+//    }
+
+    public ResourceStatus getResourceStatus(){
+        return null;
     }
 
 
--- src/test/java/com/codeaddi/mrc_resources/controller/service/ResourceServiceTests.java
+++ src/test/java/com/codeaddi/mrc_resources/controller/service/ResourceServiceTests.java
@@ -1,12 +1,14 @@
 package com.codeaddi.mrc_resources.controller.service;
 
+import com.codeaddi.mrc_resources.model.repository.entity.ResourceInUse;
 import com.codeaddi.mrc_resources.testUtils.TestData;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class ResourceServiceTests {
@@ -15,16 +17,25 @@ public class ResourceServiceTests {
     ResourceService resourceService;
 
     @Test
-    void isInUseOnDate_matchingDates_returnsTrue(){
+    void isInUseOnDate_matchingDates_returnsTrue() {
         assertTrue(resourceService.anyInUseOnDate(TestData.dateNow, TestData.ResourcesInUse.allResourcesInUse));
     }
 
     @Test
-    void isInUseOnDate_nonMatchingDates_returnsFalse(){
+    void isInUseOnDate_nonMatchingDates_returnsFalse() {
         assertFalse(resourceService.anyInUseOnDate(TestData.dateNeverUsed, TestData.ResourcesInUse.allResourcesInUse));
-
     }
 
+    @Test
+    void getThoseInUseOnDate_todaysDateWithMatchingResources_returnsOnlyThoseForToday() {
+      List<ResourceInUse> resourceInUseList =  resourceService.getThoseInUseOnDate(TestData.dateNow, TestData.ResourcesInUse.allResourcesInUse);
+
+      assertEquals(2, resourceInUseList.size());
+
+        for(ResourceInUse resource : resourceInUseList){
+            assertTrue(resource.getDate().equals(TestData.dateNow));
+        }
+    }
 
 
 }
