d477cff80705ce0ec66293375f66194e004727c6
AdelaideBaron <baronadelaide@hotmail.com>
2024-09-02 15:40:05 +0100

    formatting

--- src/main/java/com/codeaddi/mrc_resources/model/repository/entity/ResourceInUse.java
+++ src/main/java/com/codeaddi/mrc_resources/model/repository/entity/ResourceInUse.java
@@ -1,12 +1,10 @@
 package com.codeaddi.mrc_resources.model.repository.entity;
 
-
 import com.codeaddi.mrc_resources.model.enums.EquipmentType;
 import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalTime;
 import java.util.Date;
+import lombok.*;
 
 @Entity
 @Table(name = "resources_in_use")
@@ -17,31 +15,31 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResourceInUse {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id",insertable=false, updatable=false)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", insertable = false, updatable = false)
+  private Long id;
 
-    @Column(name = "id")
-    private Long resource_id; // for the boat, or blade
+  @Column(name = "id")
+  private Long resource_id; // for the boat, or blade
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "type")
-    private EquipmentType equipmentType;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "type")
+  private EquipmentType equipmentType;
 
-    @Column(name = "quantity")
-    private Integer quantity;
+  @Column(name = "quantity")
+  private Integer quantity;
 
-    @Column(name = "upcoming_session_id")
-    private Long upcomingSessionId;
+  @Column(name = "upcoming_session_id")
+  private Long upcomingSessionId;
 
-    @Column(name = "date", nullable = false)
-    @Temporal(TemporalType.DATE)
-    private Date date;
+  @Column(name = "date", nullable = false)
+  @Temporal(TemporalType.DATE)
+  private Date date;
 
-    @Column(name = "start_time")
-    private LocalTime startTime;
+  @Column(name = "start_time")
+  private LocalTime startTime;
 
-    @Column(name = "end_time")
-    private LocalTime endTime;
+  @Column(name = "end_time")
+  private LocalTime endTime;
 }
--- src/test/java/com/codeaddi/mrc_resources/testUtils/TestData.java
+++ src/test/java/com/codeaddi/mrc_resources/testUtils/TestData.java
@@ -7,7 +7,6 @@ import com.codeaddi.mrc_resources.model.enums.RowerLevel;
 import com.codeaddi.mrc_resources.model.repository.entity.Blade;
 import com.codeaddi.mrc_resources.model.repository.entity.Boat;
 import com.codeaddi.mrc_resources.model.repository.entity.ResourceInUse;
-
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -17,8 +16,12 @@ import java.util.List;
 
 public class TestData {
   private static Instant instantNow = Instant.now();
-  private static Instant specificDateInstant = Instant.ofEpochMilli(LocalDateTime.of(2025, 1, 1, 0, 0).toInstant(ZoneOffset.UTC).toEpochMilli());
-  private static Instant dateInstantNeverUsed = Instant.ofEpochMilli(LocalDateTime.of(2026, 1, 1, 0, 0).toInstant(ZoneOffset.UTC).toEpochMilli());
+  private static Instant specificDateInstant =
+      Instant.ofEpochMilli(
+          LocalDateTime.of(2025, 1, 1, 0, 0).toInstant(ZoneOffset.UTC).toEpochMilli());
+  private static Instant dateInstantNeverUsed =
+      Instant.ofEpochMilli(
+          LocalDateTime.of(2026, 1, 1, 0, 0).toInstant(ZoneOffset.UTC).toEpochMilli());
   public static Date dateNeverUsed = Date.from(dateInstantNeverUsed);
   public static Date dateNow = Date.from(instantNow);
 
@@ -34,13 +37,49 @@ public class TestData {
   public static Blade blade1 =
       Blade.builder().name("Purples").amount(8).status(EquipmentStatus.WORKING).build();
 
+  public class ResourcesInUse {
+    public static ResourceInUse bladeResourceToday =
+        ResourceInUse.builder()
+            .resource_id(1L)
+            .equipmentType(EquipmentType.BLADE)
+            .quantity(4)
+            .upcomingSessionId(1L)
+            .date(dateNow)
+            .startTime(LocalTime.NOON)
+            .endTime(LocalTime.MIDNIGHT)
+            .build();
+    public static ResourceInUse bladeResourceSetDate =
+        ResourceInUse.builder()
+            .resource_id(1L)
+            .equipmentType(EquipmentType.BLADE)
+            .quantity(4)
+            .upcomingSessionId(2L)
+            .date(specificDate)
+            .startTime(LocalTime.NOON)
+            .endTime(LocalTime.MIDNIGHT)
+            .build();
+    public static ResourceInUse boatResourceToday =
+        ResourceInUse.builder()
+            .resource_id(1L)
+            .equipmentType(EquipmentType.BOAT)
+            .quantity(4)
+            .upcomingSessionId(1L)
+            .date(dateNow)
+            .startTime(LocalTime.NOON)
+            .endTime(LocalTime.MIDNIGHT)
+            .build();
+    public static ResourceInUse boatResourceSetDate =
+        ResourceInUse.builder()
+            .resource_id(1L)
+            .equipmentType(EquipmentType.BOAT)
+            .quantity(4)
+            .upcomingSessionId(1L)
+            .date(specificDate)
+            .startTime(LocalTime.NOON)
+            .endTime(LocalTime.MIDNIGHT)
+            .build();
 
-  public class ResourcesInUse{
-    public static ResourceInUse bladeResourceToday = ResourceInUse.builder().resource_id(1L).equipmentType(EquipmentType.BLADE).quantity(4).upcomingSessionId(1L).date(dateNow).startTime(LocalTime.NOON).endTime(LocalTime.MIDNIGHT).build();
-    public static ResourceInUse bladeResourceSetDate = ResourceInUse.builder().resource_id(1L).equipmentType(EquipmentType.BLADE).quantity(4).upcomingSessionId(2L).date(specificDate).startTime(LocalTime.NOON).endTime(LocalTime.MIDNIGHT).build();
-    public static ResourceInUse boatResourceToday = ResourceInUse.builder().resource_id(1L).equipmentType(EquipmentType.BOAT).quantity(4).upcomingSessionId(1L).date(dateNow).startTime(LocalTime.NOON).endTime(LocalTime.MIDNIGHT).build();
-    public static ResourceInUse boatResourceSetDate = ResourceInUse.builder().resource_id(1L).equipmentType(EquipmentType.BOAT).quantity(4).upcomingSessionId(1L).date(specificDate).startTime(LocalTime.NOON).endTime(LocalTime.MIDNIGHT).build();
-
-    public static List<ResourceInUse> allResourcesInUse = List.of(boatResourceToday, bladeResourceToday, bladeResourceSetDate, boatResourceSetDate);
+    public static List<ResourceInUse> allResourcesInUse =
+        List.of(boatResourceToday, bladeResourceToday, bladeResourceSetDate, boatResourceSetDate);
   }
 }
