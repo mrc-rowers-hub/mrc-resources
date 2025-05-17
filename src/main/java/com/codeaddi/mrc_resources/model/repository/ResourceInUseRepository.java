db842767c0f278ec5144b1140faa999eb45ce534
AdelaideBaron <baronadelaide@hotmail.com>
2025-05-17 19:59:16 +0100

    adding methods to repository

--- src/main/java/com/codeaddi/mrc_resources/model/repository/ResourceInUseRepository.java
+++ src/main/java/com/codeaddi/mrc_resources/model/repository/ResourceInUseRepository.java
@@ -3,4 +3,12 @@ package com.codeaddi.mrc_resources.model.repository;
 import com.codeaddi.mrc_resources.model.repository.entity.ResourceInUse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ResourceInUseRepository extends JpaRepository<ResourceInUse, Long> {}
+import java.util.Date;
+import java.util.List;
+import java.time.LocalTime;
+
+public interface ResourceInUseRepository extends JpaRepository<ResourceInUse, Long> {
+    List<ResourceInUse> findByDateNot(Date date);
+    List<ResourceInUse> findByDateAndStartTimeAfter(Date date, LocalTime startTime);
+    List<ResourceInUse> findByDateAndEndTimeBefore(Date date, LocalTime endTime);
+}
